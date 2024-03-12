563524b4a4a1696c761b6a7edaee569f0fe866e3
leticiaanjos2 <161889637+leticiaanjos2@users.noreply.github.com>
2024-03-12 18:47:00 -0300

    ok

--- TRUQUINHO.js
+++ TRUQUINHO.js
@@ -1,3 +1,24 @@
+// Importando a biblioteca readline-sync
+const readlineSync = require('readline-sync');
+
+let senhaCorreta = '1234';
+let senhaDigitada;
+
+// Loop do-while para solicitar a senha até que seja correta
+do {
+    // Solicita ao usuário que insira a senha
+    senhaDigitada = readlineSync.question('Digite sua senha: ', {
+        hideEchoBack: true // Para ocultar a senha digitada
+    });
+
+    // Verifica se a senha está correta
+    if (senhaDigitada !== senhaCorreta) {
+        console.log('Senha incorreta. Tente novamente.');
+    }
+} while (senhaDigitada !== senhaCorreta);
+
+// Se o loop terminou, significa que a senha foi correta
+console.log('BEM-VINDO!');
 const manilhas = ['Ouros', 'Espadas', 'Copas', 'Paus'];
 const forcas = ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3'];
 
